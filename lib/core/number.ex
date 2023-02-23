ec980642aba384055b0a39dbd2777c39c6b390db
Kamaro <kamaroly@gmail.com>
2023-02-23 23:20:21 +0300

    Finished Core.Number module implementation

--- lib/core/number.ex
+++ lib/core/number.ex
@@ -1,6 +1,8 @@
 defmodule Core.Number do
   @moduledoc """
-  A module to deal with Random numbers.
+  A module to generate with Random numbers for Elixir Faker.
+  
+  Inspired by https://github.com/FakerPHP/Faker/blob/main/src/Faker/Core/Number.php
   """
 
   @doc """
@@ -62,5 +64,39 @@ defmodule Core.Number do
   """
   def random_digit_not_zero() do
     number_between(1, 9)
+  end
+
+  @doc """
+  Generates a random float number based on provided parameters.
+  
+  Returns `Float`.
+  
+  ## Examples
+  
+      iex> Core.Number.random_float()
+      87.3
+  """
+
+  def random_float(maximum_decimals \\ 0, min_number \\ 0, max_number \\ 0) do
+    # Set default maximum number if not provided
+    number = random_maximum(min_number, max_number) |> Integer.to_string()
+    fractions = random_maximum() |> Integer.to_string()
+
+    float_string = number <> "." <> fractions
+
+    {parsed_float, _} = Float.parse(float_string)
+
+    Float.round(parsed_float, maximum_decimals)
+  end
+
+  def swap_numbers(number_one, number_two) do
+    {number_two, number_one}
+  end
+
+  @doc """
+  Generates largest possible random value
+  """
+  def random_maximum(min \\ 2_147_000_000, max \\ 2_147_483_647) do
+    number_between(min, max)
   end
 end
--- test/core/number_test.exs
+++ test/core/number_test.exs
@@ -25,5 +25,24 @@ defmodule Core.NumberTest do
       assert results > 0
       assert results < 10
     end
+
+    test "random_float generates a random float number" do
+      results = Number.random_float()
+      assert is_float(results)
+    end
+
+    test "swap_numbers exchanges number positions" do
+      {number_1, number_2} = Number.swap_numbers(9, 7)
+
+      assert number_2 == 9
+      assert number_1 == 7
+    end
+
+    test "random_maximum generates the latest random number possible" do
+      results = Number.random_maximum()
+
+      assert results >= 2_147_000_000
+      assert results <= 2_147_483_647
+    end
   end
 end
