2a6ab8216580b888fa46d6b1f603619a8dbd7a73
Kamaro Lambert <kamaro.lambert@amitruck.com>
2023-09-19 21:27:32 +0300

    Updated version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirFaker.MixProject do
   def project do
     [
       app: :faker,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
